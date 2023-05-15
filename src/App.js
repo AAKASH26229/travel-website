0692bf17df9ffc74061b71cead2503561a2ef9a0
Aakash <parateakash21.ap@gmail.com>
2023-05-16 02:05:19 +0530

    creating body section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
+import Home from './components/pages/Home';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
     <Router>
       <Navbar />
      <Routes>
-      <Route path='/' exact />
+      <Route path='/' exact Component={Home}/>
       </Routes> 
 
     </Router>
