662311b722304521b9be6f78bfc60ca95c8b623d
Aakash <parateakash21.ap@gmail.com>
2023-05-16 15:53:38 +0530

    Adding services and products

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import Signup from './components/pages/Signup';
 
 function App() {
   return (
@@ -12,6 +15,10 @@ function App() {
       <Navbar />
      <Routes>
       <Route path='/' exact Component={Home}/>
+      <Route path='/services' Component={Services} />
+      <Route path='/products' Component={Products} />
+      <Route path='/sign-up' Component={Signup} />
+
       </Routes> 
 
     </Router>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,11 +10,11 @@ function Cards() {
             <div className='cards__wrapper'>
                 <ul className='cards__items'>
                     <CardItem src="images/img-9.jpg" text="Explore the hidden waterfall deep inside the Amazon Jungle" lable="Adventure" path="/services" />
-                    <CardItem src="images/img-2.jpg" text="Travel through the Island of Bali in a Private Cruise" lable="Luxury" path="/services" />
+                    <CardItem src="images/img-2.jpg" text="Travel through the Island of Bali in a Private Cruise" lable="Luxury" path="/products" />
                 </ul>
                 <ul className='cards__items'>
                     <CardItem src="images/img-3.jpg" text="Set Sail in the Atlantic Ocean visiting Uncharted Waters" lable="Mystery" path="/services" />
-                    <CardItem src="images/img-4.jpg" text="Experience Football on Top of the Himalayan Mountains" lable="Adventure" path="/services" />
+                    <CardItem src="images/img-4.jpg" text="Experience Football on Top of the Himalayan Mountains" lable="Adventure" path="/products" />
                     <CardItem src="images/img-8.jpg" text="Ride through the Sahara Desert on a guided camel tour" lable="Adventure" path="/services" />
 
                 </ul>
